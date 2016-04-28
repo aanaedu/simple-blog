f49bd74e88f442e2290dc3ba0e310ce40fc1a7d0
Anthony Anaedu <tony.anaedu@gmail.com>
2016-04-28 13:27:58 +0100

    Add post form and template

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
@@ -9,3 +10,17 @@ def post_list(request):
 def post_detail(req, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(req, 'post/post_detail.html', {'post': post})
+
+def post_new(req):
+	if req.method == "POST":
+		form = PostForm(req.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = req.user
+			post.published_at = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+
+	return render(req, 'post/post_edit.html', {'form': form})
